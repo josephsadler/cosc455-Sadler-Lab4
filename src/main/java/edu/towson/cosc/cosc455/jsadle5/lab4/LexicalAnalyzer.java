82d98337a0318e4b04d9fc5ac54f959c97032e98
jsadle5 <jsadle5@students.towson.edu>
2016-09-22 12:01:12 -0400

    Task 4

--- src/main/java/edu/towson/cosc/cosc455/jsadle5/lab4/LexicalAnalyzer.java
+++ src/main/java/edu/towson/cosc/cosc455/jsadle5/lab4/LexicalAnalyzer.java
@@ -68,6 +68,8 @@ public class LexicalAnalyzer {
         lexems.add("\n");lexems.add("teh"); lexems.add("a");
         lexems.add("dawg"); lexems.add("kat"); lexems.add("rat");
         lexems.add("ates"); lexems.add("lovez"); lexems.add("hatez");
+
+		lexems.add("happy"); lexems.add("fat"); lexems.add("mean"); lexems.add("hungry");
     }
 
     private boolean lookup(String candidateToken){
--- src/main/scala/edu/towson/cosc/cosc455/jsadle5/lab4/SyntaxAnalyzer.scala
+++ src/main/scala/edu/towson/cosc/cosc455/jsadle5/lab4/SyntaxAnalyzer.scala
@@ -1,8 +1,5 @@
 package edu.towson.cosc.cosc455.jsadle5.lab4
 
-import java.lang
-
-import edu.towson.cosc.cosc455.jsadle5.lab4
 
 class SyntaxAnalyzer {
 
@@ -10,6 +7,7 @@ class SyntaxAnalyzer {
   val ARTICLES : List[String] = List("teh", "a")
   val VERBS : List[String] = List("ates", "lovez", "hatez")
   val NOUNS : List[String] = List("kat", "dawg", "rat")
+  val ADJECTIVES  : List[String] = List("happy", "mean", "fat", "hungry")
 
   // Flag for errors and helper methods
   var errorFound : Boolean = false
@@ -29,35 +27,45 @@ class SyntaxAnalyzer {
   // This method implements the BNF rule for a noun phrase <NP> ::= <A> <N>
   def NounPhrase() = {
     if(!errorFound) Article()
+    if(!errorFound) Adjective()
     if(!errorFound) Noun()
+  }
+
+  def Adjective() = {
+    if (ADJECTIVES contains Compiler.currentToken)
+      Compiler.Scanner.getNextToken()
+    else {
+      println("SYNTAX ERROR - An adjective was expected when '" + Compiler.currentToken + "' was found.")
+      setError()
+    }
   }
 
   // This method implements the BNF rule for a verb <V> ::= ates | hatez | hatez
   def Verb() = {
-    if (VERBS contains lab4.Compiler.currentToken)
-      lab4.Compiler.Scanner.getNextToken()
+    if (VERBS contains Compiler.currentToken)
+      Compiler.Scanner.getNextToken()
     else {
-      println("SYNTAX ERROR - A verb was expected when '" + lab4.Compiler.currentToken + "' was found.")
+      println("SYNTAX ERROR - A verb was expected when '" + Compiler.currentToken + "' was found.")
       setError()
     }
   }
 
   // This method implements the BNF rule for a noun <N> ::= dawg | kat | rat
   def Noun() = {
-    if (NOUNS contains lab4.Compiler.currentToken)
-      lab4.Compiler.Scanner.getNextToken()
+    if (NOUNS contains Compiler.currentToken)
+      Compiler.Scanner.getNextToken()
     else {
-        println("SYNTAX ERROR - A noun was expected when '" + lab4.Compiler.currentToken + "' was found.")
+        println("SYNTAX ERROR - A noun was expected when '" + Compiler.currentToken + "' was found.")
         setError()
       }
   }
 
   // This method implements the BNF rule for an article <N> ::= teh | a
   def Article() = {
-    if (ARTICLES contains lab4.Compiler.currentToken)
-      lab4.Compiler.Scanner.getNextToken()
+    if (ARTICLES contains Compiler.currentToken)
+      Compiler.Scanner.getNextToken()
     else {
-        println("SYNTAX ERROR - An article was expected when '" + lab4.Compiler.currentToken + "' was found.")
+        println("SYNTAX ERROR - An article was expected when '" + Compiler.currentToken + "' was found.")
         setError()
       }
   }
